bca5bcc5c0d91dda37b55e8fb389899da50e3738
XSA_DEV <XSA_DEV@unknown>
2019-01-17 10:39:21 +0000

    wip

--- db/src/sql/test.sql
+++ db/src/sql/test.sql
@@ -2,8 +2,82 @@ select count(*) from "cameras.camera" where "CamType" = 'Speed Camera'
 
 SELECT "CameraHeadRef", "SpeedLimitStd" 
  from "cameras.camera" where "CamType" = 'Speed Camera'
- 
- 
+
+
+
+------  COLOR
+DROP VIEW V_COLOR;
+CREATE VIEW  V_COLOR AS (
+SELECT SEQ as COLORSEQ, "ColorID" FROM (	
+SELECT DISTINCT * FROM 
+(SELECT TOP 1000 round(RAND(),2) as SEQ FROM OBJECTS ),
+(
+SELECT "ROWNUM", "ColorID", FREQ, SUM(FREQ) OVER (  ORDER BY "ROWNUM" ) AS FREQSEQ
+FROM (
+SELECT * FROM ( SELECT ROW_NUMBER() over () as "ROWNUM", "ColorID", round("OccurenceAvg" / sum("OccurenceAvg") OVER () , 2)   as FREQ
+from "cameras.vehicleColor" ) WHERE FREQ > 0)
+) 
+WHERE ( SEQ > (FREQSEQ - FREQ)  ) -- OR  ( SEQ = 0 AND FREQ = 0) )
+and   SEQ <= FREQSEQ
+ORDER BY "ROWNUM", "ColorID", "FREQSEQ"
+)
+ORDER BY SEQ
+)
+;
+
+SELECT * FROM V_COLOR;
+
+------  TYPE
+DROP VIEW V_TYPE;
+CREATE VIEW  V_TYPE AS (
+SELECT SEQ as TYPESEQ, "TypeID" FROM (	
+SELECT DISTINCT * FROM 
+(SELECT TOP 1000000 round(RAND(),4) as SEQ FROM OBJECTS ),
+(
+SELECT "ROWNUM", "TypeID", FREQ, SUM(FREQ) OVER (  ORDER BY "ROWNUM" ) AS FREQSEQ
+FROM (
+SELECT * FROM ( SELECT ROW_NUMBER() over () as "ROWNUM", "TypeID", round("OccurenceAvg" / sum("OccurenceAvg") OVER () , 4)   as FREQ
+from "cameras.vehicleType" )  WHERE FREQ > 0)
+) 
+WHERE ( SEQ > (FREQSEQ - FREQ)  ) -- OR  ( SEQ = 0 AND FREQ = 0) )
+and   SEQ <= FREQSEQ
+ORDER BY "ROWNUM", "TypeID", "FREQSEQ"
+)
+ORDER BY SEQ
+)
+;
+
+SELECT * FROM V_TYPE;
+------  MAKE
+DROP VIEW V_MAKE;
+CREATE VIEW  V_MAKE AS (
+SELECT SEQ as MAKESEQ, "MakeID" FROM (	
+SELECT DISTINCT * FROM 
+(SELECT TOP 1000 round(RAND(),2) as SEQ FROM OBJECTS ),
+(
+SELECT "ROWNUM", "MakeID", FREQ, SUM(FREQ) OVER (  ORDER BY "ROWNUM" ) AS FREQSEQ
+FROM (
+SELECT * FROM ( SELECT ROW_NUMBER() over () as "ROWNUM", "MakeID", round("CNT" / sum("CNT") OVER () , 2)   as FREQ
+from ( SELECT * , 1 as "CNT" FROM "cameras.vehicleMake" ) ) WHERE FREQ > 0)
+) 
+WHERE ( SEQ > (FREQSEQ - FREQ)  ) -- OR  ( SEQ = 0 AND FREQ = 0) )
+and   SEQ <= FREQSEQ
+ORDER BY "ROWNUM", "MakeID", "FREQSEQ"
+)
+ORDER BY SEQ
+)
+;
+
+DROP VIEW V_MAKE;
+CREATE VIEW  V_MAKE AS ( SELECT ROW_NUMBER() over () as "SEQ", "MakeID" FROM "cameras.vehicleMake" );
+
+SELECT * FROM V_MAKE;
+SELECT COUNT(*) FROM "cameras.vehicleMake";
+SELECT distinct ROUND(RAND()*76,0) FROM OBJECTS;
+
+------------------
+
+
  SELECT 'ABCD_____'  || to_char(round(RAND()*50000000,0)) || '_XYZ' as "CHAR_COLKEY"  FROM DUMMY;
  
  SELECT DISTINCT A FROM (
